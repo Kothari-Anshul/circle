d0ac498d8b7f9a81f07fbb7c72c0d1df6d48703b
Anshul Kothari <anshulkothari96@gmail.com>
2018-03-22 11:41:15 +0530

    initial commit

---
+++ test/app.test.js
@@ -0,0 +1,14 @@
+var obj = require('../index.js');
+var chai = require('chai');
+var assert = chai.assert;
+
+describe('Equality of numbers ', function(){
+	it('should be sum of the given parameters', function(){
+		assert.equal(obj.sum(7,8), 15);
+		
+	});
+	it('should return mutiplication of the given numbers', function(){
+		assert.equal(obj.multiply(2,2), 4);
+	});
+});
+
